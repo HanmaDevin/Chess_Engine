40e18728b5bf40f5091add2951a71eba342a61e8
DevinHanma <devinbc12@icloud.com>
2024-03-25 09:24:08 +0100

    added board representation

---
+++ Chess/src/main/GameState.py
@@ -0,0 +1,15 @@
+class GameState:
+    def __init__(self):
+        # the board has standard chess dimensions. 'b' or 'w' indicate the color of the piece.
+        # the pieces are represented by 2 characters, one lowercase and one uppercase
+        # the notation is oriented by the standard chess notation
+        # 'K' = King, 'Q' = Queen, 'R' = Rook, 'B' = Bishop, 'N' = Knight, 'P' = Pawn
+        self.board = [
+            ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
+            ["bP", "bP", "bP", "bP", "bP", "bP", "bP", "bP"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["wP", "wP", "wP", "wP", "wP", "wP", "wP", "wP"],
+            ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]]

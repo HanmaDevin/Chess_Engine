e66f15b233683f26affafbd41a0d29a37f640040
HanmaDevin <devinbc12@icloud.com>
2024-03-26 15:54:05 +0100

    renamed pawn images, now matches code. for simpler access

--- Chess/src/main/GameState.py
+++ Chess/src/main/GameState.py
@@ -4,6 +4,7 @@ class GameState:
         # the pieces are represented by 2 characters, one lowercase and one uppercase
         # the notation is oriented by the standard chess notation
         # 'K' = King, 'Q' = Queen, 'R' = Rook, 'B' = Bishop, 'N' = Knight, 'P' = Pawn
+        # '--' represents an empty space
         self.board = [
             ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
             ["bP", "bP", "bP", "bP", "bP", "bP", "bP", "bP"],
@@ -13,3 +14,6 @@ class GameState:
             ["--", "--", "--", "--", "--", "--", "--", "--"],
             ["wP", "wP", "wP", "wP", "wP", "wP", "wP", "wP"],
             ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]]
+
+        self.whiteToMove = True
+        self.moveLog = []
